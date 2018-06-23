47e338f930becab5267be728b3ab54a52ac3e2bd
vurit <vuriti.teja@gmail.com>
2018-06-23 19:44:05 +0200

    Day 1 code upload

---
+++ Edureka day1/src/day1/Helloworld.java
@@ -0,0 +1,13 @@
+package day1;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+
+		System.out.println("short cut for output line SYSO -> CTRL and Space bar and enter");
+
+	}
+
+}
